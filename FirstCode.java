c5e07d63b1ae1aac864a8237516ef8d59cd89b1c
Omar <omaralaghbari61@gmail.com>
2025-08-08 19:14:18 +0300

    reduced and story 3.2

--- FirstCode.java
+++ FirstCode.java
@@ -1,6 +1,6 @@
 public class FirstCode{
     public static void main(String[] args){
         System.out.println("Hello, world!!");
-        System.out.print("what the helly")
+        System.out.print("what the helly!")
     }
 }
